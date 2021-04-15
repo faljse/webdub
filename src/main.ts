0e4451f2207d76877c02ace1b94fb803914dfbc6
Martin Kunz <martin.michael.kunz@gmail.com>
2021-04-16 01:25:09 +0200

    initial working groups

--- src/main.ts
+++ src/main.ts
@@ -52,10 +52,9 @@ class Main {
         let relay=config['Relay'];
         let dimmer=config['Dimmer'];
         let lightCode="";
+        let declCode="";
 
-
-        lightCode+=`Relay relay[${Object.keys(relay).length}];\r\n`;
-        lightCode+=`Dimmer dimmer[${Object.keys(dimmer).length}];\r\n`;
+        declCode+=`Relay relay[${Object.keys(relay).length}];\r\n`;
 
         let idx=0;
         for (let id in relay) {
@@ -65,6 +64,7 @@ class Main {
             lightCode+=`pinMode(${r.output}, OUTPUT);\r\n`
             idx++;
         }
+        declCode+=`Dimmer dimmer[${Object.keys(dimmer).length}];\r\n`;
 
         idx=0;
         for (let id in dimmer) {
@@ -78,31 +78,31 @@ class Main {
         let actionsetgroups=config['ActionSetGroups'];
         let actionCode="";
         let asgIdx=0;
-        actionCode+=`ActionSetGroup asg[${Object.keys(actionsetgroups).length}];\r\n`
+        declCode+=`ActionSetGroup asg[${Object.keys(actionsetgroups).length}];\r\n`
 
         for(let asg of actionsetgroups) {
             let asIdx=0;
+            declCode+=`ActionSet as${asgIdx}[${asg.actions.length}];\r\n`
             for(let actionset of asg.actions) {
-                actionCode+=`Action actions${asgIdx}_${asIdx}[${actionset.length}];\r\n`
-                actionCode+=`ActionSet as${asgIdx}_${asIdx}[${asg.actions.length}];\r\n`
+                declCode+=`Action actions${asgIdx}_${asIdx}[${actionset.length}];\r\n`
                 let aIdx=0;
                 for(let action of actionset) {
                     if(action.idRelay) {
-                        actionCode+=`actions${asgIdx}_${asIdx}[${aIdx}] = Action(&relay[${relay[action.idRelay].idx}], CmdType::${action.cmd}${action.value?(", "+action.value):""});\r\n`
+                        actionCode+=`actions${asgIdx}_${asIdx}[${aIdx}] = Action(0, &relay[${relay[action.idRelay].idx}], CmdType::${action.cmd}${action.value?(", "+action.value):""});\r\n`
                     }
                     console.log(action.idDimmer);
                     if(action.idDimmer) {
-                        actionCode+=`actions${asgIdx}_${asIdx}[${aIdx}] = Action(&dimmer[${dimmer[action.idDimmer].idx}], CmdType::${action.cmd}${action.value?(", "+action.value):""});\r\n`
+                        actionCode+=`actions${asgIdx}_${asIdx}[${aIdx}] = Action(&dimmer[${dimmer[action.idDimmer].idx}],0, CmdType::${action.cmd}${action.value?(", "+action.value):""});\r\n`
  
                     } 
                     aIdx++;
                 }
-                actionCode+=`as${asgIdx}_${asIdx}[${asIdx}]=ActionSet(1, ${actionset.length}, actions${asgIdx}_${asIdx});\r\n`
+                actionCode+=`as${asgIdx}[${asIdx}]=ActionSet(1, ${actionset.length}, actions${asgIdx}_${asIdx});\r\n`
                 asIdx++;
             }
             console.log("asg");
             console.log(asg);
-            actionCode+=`asg[${asgIdx}]=ActionSetGroup(${asg.id}, ${asg.actions.length}, as${asgIdx}_${asIdx-1});\r\n`
+            actionCode+=`asg[${asgIdx}]=ActionSetGroup(${asg.id}, ${asg.actions.length}, as${asgIdx});\r\n`
             asgIdx++;
         } 
         let buttons=config['Buttons'];
@@ -130,6 +130,8 @@ class Main {
 #include "actionsetgroup.h"
 #include "mbutton.h"
 #include "buttonaction.h"\r\n\r\n`
+
+header+=declCode;
 
         header+=`void buildConfig() {\r\n`;
 
